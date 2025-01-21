4f0e378e86d6a1d0ae16e5bc1191103e4f179f59
PPokhrel817 <pokhrel.prince@flemingcollege.ca>
2025-01-20 19:54:41 -0500

    Modified both files in the repo clone
    -Delete a print() statement from script1.py
    -Added two print() statement from script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,5 @@
 # This is good programming advice
+print("Explicit is better than implicit.")
+print("If the implementation is easy to explain, it may be a good idea.")
+print("Explicit is better than implicit.")
 print("If the implementation is easy to explain, it may be a good idea.")
