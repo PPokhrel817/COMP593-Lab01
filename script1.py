6578a6feb90a2e14863ee663d040d8234f93997e
PPokhrel817 <pokhrel.prince@flemingcollege.ca>
2025-01-20 19:27:35 -0500

    Created script files
    Added code that prints lines from Zen of  Python

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("Explicit is better than implicit.")
+print("There should be one-- and preferably only one --obvious way to do it.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("lat is better than nested.")
+print("If the implementation is easy to explain, it may be a good idea.")
