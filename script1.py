a997414e78f9d23d6cf005eed7c219dff5ee7932
PPokhrel817 <pokhrel.prince@flemingcollege.ca>
2025-01-20 19:45:07 -0500

    Modified both script files
    -Deleted print from script1.py
    -Added comment to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("Explicit is better than implicit.")
-print("There should be one-- and preferably only one --obvious way to do it.")
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# Print lines from Zen of Python
 print("lat is better than nested.")
 print("If the implementation is easy to explain, it may be a good idea.")
